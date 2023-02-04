80c40a7363de9b301b4b813aba84d5efed42dadf
Ben Aaron <ben@theaarons.com>
2023-02-04 09:35:01 -0800

    more bugs and also added deletion

--- app/cache.py
+++ app/cache.py
@@ -4,37 +4,48 @@ from typing import Any
 class MemoryFirstCache:
     """Store stuff for fast reading."""
     def __init__(self, directory: pathlib.Path, flush_interval: int=100):
-        self.store = {}
+        self.__store__ = {}
         self.directory = directory
 
     def get_val(self,key: str) -> Any:
-        val = self.store.get(key)
+        val = self.__store__.get(key)
         if val is None:
             val = self.__get_from_disk__(key)
         return val
 
-    def __get_from_memory__(key):
+    def __get_from_memory__(self, key):
         return self.val.get(key)
 
-    def __get_from_disk__(key):
+    def __get_from_disk__(self, key):
         try: 
-            val = (self.directory / key).read()
-        except Exception as exc:
-            # TODO: Don't catch broad Exception
-            raise exc
+            val = (self.directory / key).read_text()
+        except FileNotFoundError:
+            raise ValueError
         return val
     def __getitem__(self, key):
         return self.get_val(key)
 
     def put_val(self, key: str, val: Any):
-        self.store[key] = val
+        self.__store__[key] = val
         return
 
     def __setitem__(self, key, val):
         return self.put_val(key, val)
 
+    def __delitem__(self, key):
+        return self.delete_val(key)
+
+    def delete_val(self, key: str):
+        del self.__store__[key]
+        try:
+            (self.directory / key).unlink()
+        except FileNotFoundError:
+            # lol never got flushed
+            pass
+        return
+
     def flush():
-        for key, val in self.store.items():
+        for key, val in self.__store__.items():
             val_path = self.directory / key
             val_path.write(val)
         return
--- app/test_cache.py
+++ app/test_cache.py
@@ -1,13 +1,17 @@
+import pytest
 from  app.cache import MemoryFirstCache
 
 def test_cache_put_and_get(tmp_path):
     cash = MemoryFirstCache(tmp_path)
     cash["nevada"] = "Carson City"
     assert cash["nevada"] == "Carson City"
+    return
 
 def test_cache_put_get_and_del(tmp_path):
     cash = MemoryFirstCache(tmp_path)
     cash["nevada"] = "Carson City"
     assert cash["nevada"] == "Carson City"
     del cash["nevada"]
-    assert cash["nevada"] == None
+    with pytest.raises(ValueError):
+        val = cash["nevada"]
+    return
