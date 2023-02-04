dbdf24013ed22bea96d9647ba300ca66abcde42e
Ben Aaron <ben@theaarons.com>
2023-02-04 11:16:25 -0800

    test api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import os
 import pathlib
-from typing import Any
+from typing import Any, AnyStr, Dict, List, Union
 
 import fastapi
 
@@ -16,16 +16,23 @@ CACHE_SIZE = int(os.environ.get("JOHNNY_CACHE_SIZE", "10000"))
 app = fastapi.FastAPI()
 cache = cache.MemoryFirstCache(CACHE_PATH, flush_size=CACHE_SIZE)
 
+json_object = Dict[AnyStr, Any]
+json_array = List[Any]
+json_struct = Union[json_array, json_object]
+
 @app.get("/")
 async def healthcheck():
     return {"status": "Truckin'"}
 
 @app.get("/{key}")
 async def get_item(key: str):
-    return cache[key]
+    try:
+        return cache[key]
+    except KeyError:
+        raise fastapi.HTTPException(404, detail=f"{key} not found")
 
 @app.put("/{key}")
-async def put_item(key: str, value: str):
+async def put_item(key: str, value: json_struct = None):
     cache[key] = value
     return
 
--- app/test_main.py
+++ app/test_main.py
@@ -3,7 +3,44 @@ from fastapi import testclient
 
 client = testclient.TestClient(main.app)
 
-def test_healthcheck():
+def test_healthcheck(monkeypatch, tmpdir):
+    monkeypatch.setenv("JOHNNY_CACHE_PREFIX", str(tmpdir))
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"status": "Truckin'"}
+
+def test_put_and_get(monkeypatch, tmpdir):
+    monkeypatch.setenv("JOHNNY_CACHE_PREFIX", str(tmpdir))
+    response = client.put("/foo", json=["bar"])
+    assert response.status_code == 200
+    response = client.get("/foo")
+    assert response.status_code == 200
+    assert response.json() == ["bar"]
+
+def test_get_nonexistent_key(monkeypatch, tmpdir):
+    monkeypatch.setenv("JOHNNY_CACHE_PREFIX", str(tmpdir))
+    response = client.get("/doesnt-exits-ayy-lmao")
+    assert response.status_code == 404
+
+
+def test_put_twice(monkeypatch, tmpdir):
+    monkeypatch.setenv("JOHNNY_CACHE_PREFIX", str(tmpdir))
+    response1 = client.put("/foo", json={"foo": "foo"})
+    assert response1.status_code == 200
+    response2 = client.put("/foo", json={"bar": "bar"})
+    assert response2.status_code == 200
+    response3 = client.get("/foo")
+    assert response3.status_code == 200
+    assert response3.json() == {"bar": "bar"}
+
+
+def test_404_after_delete(monkeypatch, tmpdir):
+    monkeypatch.setenv("JOHNNY_CACHE_PREFIX", str(tmpdir))
+    response1= client.put("/foo", json=["bar"])
+    assert response1.status_code == 200
+    response2 = client.delete("/foo")
+    assert response2.status_code == 200
+    response3 = client.get("/foo")
+    assert response3.status_code == 404
+
+def test_put_str(monkeypatch, tmpdir)
