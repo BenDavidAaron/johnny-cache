7a3a951cf746a3097be45bd4174c4ff355e11b5e
Ben Aaron <ben@theaarons.com>
2023-02-03 18:34:28 -0800

    wait I wasn't really caching

--- main.py
+++ main.py
@@ -13,8 +13,46 @@ DISK_PREFIX.mkdir(parents=True, exist_ok=True)
 
 CACHE_SIZE = int(os.environ.get("JOHNNY_CACHE_SIZE", "1000"))
 
-app = fastapi.FastAPI()
+class MemoryFirstCache:
+    """Store stuff for fast reading."""
+    def __init__(self, directory: pathlib.Path, flush_interval: int=100):
+        self.store = {}
+        self.directory = directory
 
+    def get_val(key: str) -> Any:
+        val = self.store.get(key)
+        if val is None:
+            self.__get_from_disk__(key)
+
+    def __get_from_memory__(key):
+        return self.val.get(key)
+
+    def __get_from_disk__(key):
+        try: 
+            val = (self.directory / key).read()
+        except Exception as exc:
+            # TODO: Don't catch broad Exception
+            raise exc
+        return val
+    def __getitem__(self, key):
+        return self.get_val(key)
+
+    def put_val(key: str, val: Any):
+        self.store[key] = val
+        return
+
+    def __putitem__(self, key, val):
+        return self.put_val(key, val)
+
+    def flush():
+        for key, val in self.store.items():
+            val_path = self.directory / key
+            val_path.write(val)
+        return
+
+
+app = fastapi.FastAPI()
+cache = MemoryFirstCache(DISK_PREFIX, CACHE_SIZE)
 
 @app.get("/")
 async def healthcheck():
@@ -23,24 +61,10 @@ async def healthcheck():
 
 @app.get("/{key}")
 async def get_item(key: str):
-    return get_from_cache(key)
-
-
-def get_from_cache(key: str):
-    cache_path = DISK_PREFIX / key
-    with open(cache_path, "r") as cf:
-        return cf.read()
-
+    return cache[key]
 
 @app.put("/{key}")
 async def put_item(key: str, value: str):
-    put_in_cache(key, value)
-    return
-
-
-def put_in_cache(key, value):
-    cache_path = DISK_PREFIX / key
-    with open(cache_path, "w") as cf:
-        cf.write(value)
+    cache[key] = value
     return
 
