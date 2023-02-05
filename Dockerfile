c9acac3eccab8bf28bff8af27c345357fd659266
Ben Aaron <ben@theaarons.com>
2023-02-04 18:36:22 -0800

    make env var names better

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,6 @@ COPY app/ /opt/app/
 
 RUN pip install -r /opt/requirements.txt
 
+ENV JOHNNY_CACHE_DATA_PATH=/opt/data
+
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
--- app/config.py
+++ app/config.py
@@ -2,10 +2,11 @@ import os
 import pathlib
 
 CACHE_PATH = (
-    pathlib.Path(os.environ.get("JONNY_CACHE_PREFIX", "~/.johnny_cache"))
+    pathlib.Path(os.environ.get("JOHNNY_CACHE_DATA_PATH", "~/.johnny_cache"))
     .expanduser()
     .absolute()
 )
 CACHE_PATH.mkdir(parents=True, exist_ok=True)
 
-CACHE_SIZE = int(os.environ.get("JOHNNY_CACHE_SIZE", "10000"))
+CACHE_SIZE = int(os.environ.get("JOHNNY_CACHE_FLUSH_SIZE", "1000"))
+
