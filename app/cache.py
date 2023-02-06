d96337cffda329c6d6146b871348f1312ee011a7
Ben Aaron <ben@theaarons.com>
2023-02-06 09:57:04 -0800

    Syntactic Cleanup (#3)
    * syntactic cleanup on cache
    
    * fix GET behavior
    
    * app
    
    * whoops dunders aren't private methods

--- app/cache.py
+++ app/cache.py
@@ -12,40 +12,45 @@ class MemoryFirstCache:
         self.__store__: Dict[Any, Any] = {}
         self.__unflushed_objects__: List[Tuple[str, Any]] = []
 
-    def get_val(self, key: str) -> Any:
-        val = self.__get_from_memory__(key)
-        if val is None:
-            val = self.__get_from_disk__(key)
+    def get(self, key: str) -> Any:
+        val = self.__get_from_memory(key)
+        if val is not None:
+            return val
+        val = self.__get_from_disk(key)
         return val
 
-    def __get_from_memory__(self, key):
+    def __get_from_memory(self, key):
         return self.__store__.get(key)
 
-    def __get_from_disk__(self, key):
+    def __get_from_disk(self, key):
         try:
             val = pickle.load((self.directory / key).open("rb"))
         except FileNotFoundError as exc:
-            raise KeyError from exc
+            return None
         return val
 
     def __getitem__(self, key):
-        return self.get_val(key)
+        val = self.get(key)
+        if val is None:
+            raise KeyError
+        else:
+            return val
 
-    def put_val(self, key: str, val: Any):
+    def put(self, key: str, val: Any):
         self.__store__[key] = val
         self.__unflushed_objects__.append(("PUT", key))
         if len(self.__unflushed_objects__) > self.flush_size:
             self.flush()
 
     def __setitem__(self, key, val):
-        return self.put_val(key, val)
+        return self.put(key, val)
 
-    def __delitem__(self, key):
-        return self.delete_val(key)
-
-    def delete_val(self, key: str):
+    def delete(self, key: str):
         del self.__store__[key]
         self.__unflushed_objects__.append(("DEL", key))
+
+    def __delitem__(self, key):
+        return self.delete(key)
 
     def flush(self):
         for verb, key in self.__unflushed_objects__:
@@ -53,7 +58,7 @@ class MemoryFirstCache:
                 (self.directory / key).unlink()
             elif verb == "PUT":
                 pickle.dump(
-                    self.__get_from_memory__(key),
+                    self.__get_from_memory(key),
                     (self.directory / key).open("wb"),
                 )
 
