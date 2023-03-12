20fa7c2e407f0c14745dc364ac7766b7e0f045de
Ben Aaron <ben@theaarons.com>
2023-03-11 19:03:40 -0800

    add access for cache

---
+++ app/crud.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+from app import models
+
+
+def get_record(db: Session, key: str) -> models.Record:
+    return db.query(models.Record).filter(models.Record.key == key).first()
+
+
+def put_record(db: Session, key: str, value: bytes) -> models.Record:
+    db_record = models.Record(key=key, value=value)
+    db.add(db_record)
+    db.commit()
+    db.refresh(db_record)
+    return db_record
+
+
+def delete_record(db: Session, key: str) -> None:
+    db_record = db.query(models.Record).filter(models.Record.key == key).first()
+    db.delete(db_record)
+    db.commit()
